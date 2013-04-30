da964e9ba1bb5b5f442692b23d7db24e1fbd6f0f
David Grayson <davidegrayson@gmail.com>
2013-04-30 04:59:19 -0700

    Wrote the javascript that figures out what domains a person is interested in based on what they typed.

--- www/application.js
+++ www/application.js
@@ -1,11 +1,39 @@
 $(function() {
   var conn;
-  var userinput = $("#userinput");
-  
+  var user_input = $("#user-input");
+  var tldsOfInterest = ["com", "net"];
+  var userInputInvalid = false;
+  var domainsOfInterest = [];
+
   if (!window["WebSocket"]) {
-    alert("Sorry, your browser does not support WebSockets.  Try Google Chrome!");
+    alert("Sorry, your browser does not support websockets.  Try Google Chrome!");
+    location.href = "http://www.google.com/chrome/";
     return;
   }
+
+  user_input.keyup(function() {
+    var input_string = user_input.val().toLowerCase();
+    if (!input_string.match(/[a-z.]+/)) {
+      userInputInvalid = true;
+    }
+    else {
+      userInputInvalid = false;
+      var parts = input_string.split("\.")
+      parts = $.grep(parts, function(e){ return e; })
+      if (parts.length == 0) {
+        domainsOfInterest = []
+      }
+      else if (parts.length == 1) {
+        domainsOfInterest = $.map(tldsOfInterest, function(tld) {
+          return parts[0] + "." + tld;
+        });
+      }
+      else {
+        domainsOfInterest = [parts[parts.length-2] + "." + parts[parts.length-1]]; 
+      }
+    }
+    console.log("invalid: " + userInputInvalid + ", domains: " + domainsOfInterest);
+  });
 
   conn = new WebSocket("ws://" + window.location.hostname + ":" + window.location.port + "/ws");
   conn.onclose = function(evt) {
