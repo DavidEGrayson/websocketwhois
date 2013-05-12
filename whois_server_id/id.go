4ea69ba80ed379a1ec9507fd67aa50de32e9e310
David Grayson <davidegrayson@gmail.com>
2013-05-11 21:04:18 -0700

    Totally changed the structure so that we are grouping the lines by the server they refer to before we try o contact any servers.

--- whois_server_id/id.go
+++ whois_server_id/id.go
@@ -19,71 +19,51 @@ import (
 // This represents a line from tld_serv_list, which came from the
 // standard unix whois utility. 
 type upstreamSuffixInfo struct {
-  suffix, server, note string
+  name, server, note string
 }
 
-type niceSuffixInfo struct {
-  suffix, server, note, protocol string
+type serverInfo struct {
+  name, note, protocol string
+  suffixes []string
 }
 
-type server string
-
-func (s *server) query(query string) {
-  addr := string(*s) + ":43"
-  l := *log.New(os.Stdout, fmt.Sprintf("%s: ", addr), log.Flags())
-  l.Println("Dialing")
+func (s *serverInfo) query(query string) {
+  addr := s.name + ":43"
+  log := *log.New(os.Stdout, fmt.Sprintf("%s: ", addr), log.Flags())
   conn, err := net.DialTimeout("tcp", addr, 40 * time.Second)
   if err != nil {
-    l.Println("Error dialing", err)
+    log.Println("Error dialing", err)
     return
   }
   defer conn.Close()
 
   _, err = fmt.Fprint(conn, query + "\r\n")
   if err != nil {
-    l.Println("Error sending", err)
+    log.Println("Error sending", err)
     return
   }
 
-  
 }
 
-func identifyServer(info upstreamSuffixInfo) (output niceSuffixInfo) {
-  output.suffix = info.suffix
-  output.server = info.server
-  output.note = info.note
+func (s *serverInfo) identify() {
+  log := *log.New(os.Stdout, fmt.Sprintf("%s: ", s.name), log.Flags())
 
-  var s server = server(info.server)
+  log.Print("Identifying.")
 
-  if output.note == "WEB" {
-    // This one is only accessible from the web.
-    return
-  }
-
-  if output.note == "NONE" || output.server == "" {
-    return
-  }
-
+  // Can we get a help screen?
   s.query("?")
 
   return
 }
 
-func parallelMap(input []upstreamSuffixInfo, f func(upstreamSuffixInfo) niceSuffixInfo) (output []niceSuffixInfo) {
-  output = make([]niceSuffixInfo, len(input))
-  ch := make(chan niceSuffixInfo)
-  process := func(info upstreamSuffixInfo) { ch <- f(info) }
-  for _, info := range input { go process(info) }
-  for i := range output { output[i] = <- ch }
-  return
-}
-
-// For easier debugging...
-func normalMap(input []upstreamSuffixInfo, f func(upstreamSuffixInfo) niceSuffixInfo) (output []niceSuffixInfo) {
-  output = make([]niceSuffixInfo, len(input))
-  for i, info := range input { output[i] = f(info) }
-  return
-}
+//func parallelMap(input []upstreamSuffixInfo, f func(upstreamSuffixInfo) niceSuffixInfo) (output []niceSuffixInfo) {
+//  output = make([]niceSuffixInfo, len(input))
+//  ch := make(chan niceSuffixInfo)
+//  process := func(info upstreamSuffixInfo) { ch <- f(info) }
+//  for _, info := range input { go process(info) }
+//  for i := range output { output[i] = <- ch }
+//  return
+//}
 
 func readUpstreamSuffixInfos(filename string) []upstreamSuffixInfo {
   upstreamSuffixInfos := make([]upstreamSuffixInfo, 0)
@@ -107,37 +87,89 @@ func readUpstreamSuffixInfos(filename string) []upstreamSuffixInfo {
     line = strings.Split(line, "#")[0]     // Remove comments
     fields := strings.Fields(line)         // Split by whitespace.
     
-    var info upstreamSuffixInfo
+    var suffix upstreamSuffixInfo
 
     if len(fields) == 0 {
       continue   // Empty line.
     }
 
-    info.suffix = fields[0]
+    suffix.name = fields[0]
     attrs := fields[1:]
     for _, attr := range attrs {
       if attr[0] >= 'A' && attr[0] <= 'Z' {
-        info.note = attr
+        suffix.note = attr
       } else {
-        info.server = attr
+        suffix.server = attr
       }
     }
 
-    upstreamSuffixInfos = append(upstreamSuffixInfos, info)
+    upstreamSuffixInfos = append(upstreamSuffixInfos, suffix)
   }
 
   return upstreamSuffixInfos
 }
 
+func groupByServer(suffixes []upstreamSuffixInfo) map[string] *serverInfo {
+  servers := map[string] *serverInfo { }
+  for _, suffix := range(suffixes) {
+
+    if suffix.server == "" || suffix.note == "WEB" || suffix.note == "NONE" {
+      // This entry does not have an actual whois server; ignore it.
+      continue
+    }
+
+    // The upstream file has dashes instead of dots for some weird TLDs
+    // at the bottom, like -tel.  I am not sure why.  Some of them
+    // seem to be duplicate entries.  Skip them for now.
+    // TODO: figure out what the - means.
+    if strings.HasPrefix(suffix.name, "-") {
+      continue
+    }
+
+    server := servers[suffix.server]
+
+    if server == nil {
+      server = &serverInfo{}
+      servers[suffix.server] = server
+      server.name = suffix.server
+      server.note = suffix.note
+    }
+
+    if server.note != suffix.note {
+      log.Fatalf("Conflicting notes for %s: %s and %s.",
+        server.name, server.note, suffix.note)
+    }
+    
+    server.suffixes = append(server.suffixes, suffix.name)
+  }
+
+  return servers
+}
+
+func serialIdentifyAll(servers map[string] *serverInfo) {
+  for _, server := range servers {
+    (*server).identify()
+  }
+}
+
 func main() {
-  fmt.Println("Identifying the servers...")
-
   upstreamSuffixInfos := readUpstreamSuffixInfos("tld_serv_list")
+  
+  //fmt.Println(upstreamSuffixInfos)
 
-  niceSuffixInfos := parallelMap(upstreamSuffixInfos, identifyServer)
-  //niceSuffixInfos := normalMap(upstreamSuffixInfos, identifyServer)
+  servers := groupByServer(upstreamSuffixInfos)
+
+  // TODO: Since servers only has info about actual whois servers, we
+  // should also pull out the information about TLDs that have no server
+  // or only have a web interface, so we can show it to our users should
+  // they request it.  That infor is in upstreamSuffixInfos.
+
+  //niceSuffixInfos := parallelMap(upstreamSuffixInfos, identifyServer)
+  serialIdentifyAll(servers)
   
   // TODO: sort results
 
-  fmt.Println(niceSuffixInfos);
+  for _, server := range servers {
+    fmt.Println(*server);
+  }
 }
