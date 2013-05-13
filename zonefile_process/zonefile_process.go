07ecdf0da69c9131641faabc5520aa12c8e20081
David Grayson <davidegrayson@gmail.com>
2013-05-13 15:34:39 -0700

    zonefile_process: Changed it to use the bytes package instead of the strings package.

--- zonefile_process/zonefile_process.go
+++ zonefile_process/zonefile_process.go
@@ -1,30 +1,32 @@
 package main
 
-// TODO: change this to use the 'bytes' package, not strings
-
 // TODO: assert that the lines that contain domain names are contiguous block
 //   If not, throw an error.
 
 // TODO: assert that no domain names are longer than 80 characters
+
+// TODO: assert that each domain name is lexigraphically after the previous
 
 import (
   "os"
   "log"
   "io"
   "bufio"
-  "strings"
+  "bytes"
 )
 
 func main() {
   log.SetOutput(os.Stderr)
   if (len(os.Args) != 2) {
-    log.Print("Usage: cat zonefile | zonefile_process tld > domainlist")
+    log.Print("Usage: zcat zonefile.gz | zonefile_process TLD > domainlist")
     os.Exit(2)
   }
 
   reader := bufio.NewReader(os.Stdin)
-  domainNameSuffix := "." + strings.ToUpper(os.Args[1]) + "."
+  domainNameSuffix := bytes.ToUpper([]byte("." + os.Args[1] + "."))
   var lastDomainName string
+
+  ns := []byte("NS")
 
   for {
     str, err := reader.ReadString('\n')
@@ -35,27 +37,31 @@ func main() {
       log.Fatal(err)
     }
 
-    fields := strings.Fields(str)
+    fields := bytes.Fields( []byte(str) )
     if len(fields) == 0 {
       continue
     }
 
-    if strings.HasSuffix(fields[0], domainNameSuffix) &&
-      fields[1] == "NS" && fields[0] != lastDomainName {
+    if bytes.HasSuffix(fields[0], domainNameSuffix) && bytes.Compare(fields[1], "NS") == 0 {
 
-      // Found a new domain name.  Write it to stdout.
-      lastDomainName = fields[0]
-
-      domainName := []byte(strings.ToLower(fields[0]))
-
-      // Replace the TLD with newline.  ".org." -> "\n"
+      // Found a domain name.  Process it.
+      domainName := bytes.ToLower(fields[0])
       domainName = domainName[0 : len(domainName) - len(domainNameSuffix) + 1]
       domainName[len(domainName)-1] = '\n'
+      
+      comparison = bytes.Compare(fields[0], lastDomainName)
+
+      if comparison == 0 {
+        // We already printed this domain name.
+        continue
+      }
 
       _, err := os.Stdout.Write(domainName)
       if err != nil {
         log.Fatal(err);
       }
+
+      lastDomainName = domainName
     }
 
   }
