ee16fca2ae277378aa599048bb06a5f75228a790
David Grayson <davidegrayson@gmail.com>
2013-05-13 15:58:53 -0700

    zonefile_process: Made unexpected sorting be a fatal error.

--- data_process.sh
+++ data_process.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
+# TODO: find a smooth way to replace data/org.domains while the go process is still running.
+# It should be able to use the old file for a while longer, but detect the new file.
 zcat data/org.zone.gz | zonefile_process/zonefile_process org > data/org.domains
