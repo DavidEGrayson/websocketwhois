4215499d5e58bc9304cfd233caddd70ae60f5f30
David Grayson <davidegrayson@gmail.com>
2013-05-13 12:56:07 -0700

    Added more benchmarks to get a better sense.  Most of them are actually about 80ms per op but the graysonfamily lookup is 120ms.

--- domainlist/domainlist_test.go
+++ domainlist/domainlist_test.go
@@ -4,13 +4,28 @@ import (
   "testing"
 )
 
+func BenchmarkLookupMid(b *testing.B) {
+  benchLookup(b, "graysonfamily")
+}
 
-func BenchmarkMidLookup(b *testing.B) {
+func BenchmarkLookupMid2(b *testing.B) {
+  benchLookup(b, "rubyonrails")
+}
+
+func BenchmarkLookupStart(b *testing.B) {
+  benchLookup(b, "0")
+}
+
+func BenchmarkLookupEnd(b *testing.B) {
+  benchLookup(b, "zzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz")
+}
+
+func benchLookup(b *testing.B, entry string) {
   list, err := Open("../data/org.domains")  
   if err != nil { b.Fail() }
   b.ResetTimer()
   for i := 0; i < b.N; i++ {
-    offset, err := list.Find("graysonfamily")
+    offset, err := list.Find(entry)
     if err != nil || offset < 0 { b.Fail() }
   }
 }
